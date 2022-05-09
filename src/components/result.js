fab95e21631c930de232ac997403f78352e25874
shaham007 <shaham.prince56@gmail.com>
2022-05-09 15:37:44 +0200

    map function correction

--- src/components/result.js
+++ src/components/result.js
@@ -2,25 +2,29 @@ import React from 'react';
 import {useLocation} from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
 
+
 const Result = () => {
     const {state: {resultSet}} = useLocation();
     return(
      <div className="result">
          <div className="card">
-            <h1>Results :</h1>
-            <div>{Object.keys(resultSet).map((p,i)=>{
-            
-            <div key={i} className="resultContainer">
-                <div className='resultProgress'>
-                    <h2>{p}:</h2>
-                    <CircularProgress variant="determinate" value={resultSet[p]} />
-                    <h2>{resultSet[p]}%</h2>
-                </div>
-            </div>
-            })};
-            
-            </div>
-         </div>  
+         <h1>Results :</h1>
+         <div>{Object.keys(resultSet).map((p,i)=>
+         
+         <div key={i} className="resultContainer">
+             <div className='resultProgress'>
+                 <h2>{p}:</h2>
+                <CircularProgress variant="determinate" value={resultSet[p]} />
+                <h2>{resultSet[p]}%</h2>
+             </div>
+        </div>
+         
+         )}
+         
+         </div>
+        
+         </div>
+        
     </div>
 )};
 
